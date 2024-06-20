f785ba9745aeb666b5144535f1308380c28d57e1
Mathai Mathew Pulicken <49433106+mathewp88@users.noreply.github.com>
2024-06-20 08:33:47 +0530

    barely functional hyprland

--- home-manager/modules/hypr/hyprland.nix
+++ home-manager/modules/hypr/hyprland.nix
@@ -1,6 +1,33 @@
-{ pkgs, lib, inputs, ... }:
+{config, pkgs, lib, inputs, ... }:
 {
+
+  home.packages = with pkgs; [
+    swww
+  ];
+
   wayland.windowManager.hyprland = {
     enable = true;
+    settings = {
+
+      "$terminal" = "wezterm";
+      "$browser" = "firefox";
+      "$mainMod" = "SUPER";
+
+      exec-once = [
+        "swww-daemon"
+      ];
+
+      exec = [
+        "swww img ${config.stylix.image}"
+      ];
+
+      misc.disable_hyprland_logo = true;
+      misc.disable_splash_rendering = true;
+
+      bind = [
+        "$mainMod, return, exec, $terminal"
+        "$mainMod, I, exec, $browser"
+      ];
+    };
   };
 }
