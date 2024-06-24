5040fd0500cf017f86c86eb80bcead05a0cb35b0
Mathai Mathew Pulicken <49433106+mathewp88@users.noreply.github.com>
2024-06-25 02:24:18 +0530

    Added rofi launcher

---
+++ home-manager/modules/hypr/rofi/rofi.nix
@@ -0,0 +1,22 @@
+{ config, ... }:
+
+with config.lib.stylix.colors;
+{  
+  #programs.rofi.enable = true;
+
+  home.file.".config/rofi/config.rasi".source = ./config.rasi;
+
+  home.file.".config/rofi/launch.rasi".source = ./launch.rasi;
+  
+  home.file.".config/rofi/color.rasi".text = ''
+    * {
+        font: "JetBrains Mono Nerd Font 13";
+        background:     #${base00};
+        background-alt: #${base01};
+        foreground:     #${base05};
+        selected:       #${base0D};
+        active:         #${base0D};
+        urgent:         #${base09};
+    }
+  '';
+}
