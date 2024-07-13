228c3226884ad67b77f0335354f0ef0d81fc6227
Mathai Mathew Pulicken <49433106+mathewp88@users.noreply.github.com>
2024-07-14 03:50:08 +0530

    Fixed login keyring issue

--- nixos/modules/hypr/hyprland.nix
+++ nixos/modules/hypr/hyprland.nix
@@ -1,4 +1,4 @@
-{ lib, pkgs, config, ... }:
+{ lib, pkgs, ... }:
 {
   services.xserver.displayManager.gdm.enable = true;
   programs.hyprland = {
@@ -7,11 +7,12 @@
   };
 
   xdg.portal.enable = true;
-  #xdg.portal.extraPortals = [ pkgs.xdg-desktop-portal-gtk ];
+  # xdg.portal.extraPortals = [ pkgs.xdg-desktop-portal-gtk ];
 
   security.pam.services.gdm.enableGnomeKeyring = true;
+  security.pam.services.gdm-password.enableGnomeKeyring = true;
   security.pam.services.hyprlock.text = lib.readFile "${pkgs.hyprlock}/etc/pam.d/hyprlock";
-  
+
   environment.systemPackages = with pkgs; [
     polkit_gnome # For auth in wm
     cinnamon.nemo-with-extensions
