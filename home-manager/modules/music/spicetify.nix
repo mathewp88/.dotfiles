d927e3e21a76663d5160a687b940d4c34e752505
Mathai Mathew Pulicken <49433106+mathewp88@users.noreply.github.com>
2024-07-24 08:09:24 +0530

    Using Greg-L's spicetify-nix

--- home-manager/modules/music/spicetify.nix
+++ home-manager/modules/music/spicetify.nix
@@ -1,53 +1,21 @@
-{ config, pkgs, lib, spicetify-nix, ... }:
+{ inputs, pkgs, ... }:
 
-with config.lib.stylix.colors;
-let
-  spicePkgs = spicetify-nix.packages.${pkgs.system}.default;
-in
 {
-  # allow spotify to be installed if you don't have unfree enabled already
-  nixpkgs.config.allowUnfreePredicate = pkg: builtins.elem (lib.getName pkg) [
-    "spotify"
-  ];
-
   # import the flake's module for your system
-  imports = [ spicetify-nix.homeManagerModule ];
+  imports = [ inputs.spicetify-nix.homeManagerModules.default ];
 
-  # configure spicetify :)
   programs.spicetify =
+    let
+      spicePkgs = inputs.spicetify-nix.legacyPackages.${pkgs.system};
+    in
     {
       enable = true;
-      # custom Dribbblish theme
-      theme = spicePkgs.themes.Dribbblish;
-
-      # specify that we want to use our custom colorscheme
-      colorScheme = "custom";
-
-      # color definition for custom color scheme. (rosepine)
-      customColorScheme = {
-        text = "${base0A}";
-        subtext = "${base05}";
-        sidebar-text = "${base05}";
-        main = "${base00}";
-        sidebar = "${base01}";
-        player = "${base00}";
-        card = "${base00}";
-        shadow = "${base01}";
-        selected-row = "${base03}";
-        button = "${base0B}";
-        button-active = "${base0F}";
-        button-disabled = "${base0F}";
-        tab-active = "${base0F}";
-        notification = "${base0C}";
-        notification-error = "${base08}";
-        misc = "${base03}";
-      };
-
-
       enabledExtensions = with spicePkgs.extensions; [
-        fullAppDisplay
-        shuffle # shuffle+ (special characters are sanitized out of ext names)
+        adblock
         hidePodcasts
+        shuffle # shuffle+ (special characters are sanitized out of extension names)
       ];
+      theme = spicePkgs.themes.ziro;
+      colorScheme = "rose-pine";
     };
 }
