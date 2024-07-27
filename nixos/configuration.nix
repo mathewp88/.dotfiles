876ac36f3f6f52130f20035decac873c8288fb1c
Mathai Mathew Pulicken <49433106+mathewp88@users.noreply.github.com>
2024-07-27 07:20:36 +0530

    Add nix.optimise to save space

--- nixos/configuration.nix
+++ nixos/configuration.nix
@@ -54,9 +54,12 @@
   programs.nh = {
     enable = true;
     clean.enable = true;
-    clean.extraArgs = "--keep-since 4d --keep 3";
+    clean.dates = "daily";
+    clean.extraArgs = "--keep-since 7d --keep 10";
     flake = "/home/mathai/.dotfiles";
   };
+
+  nix.optimise.automatic = true;
 
   system.stateVersion = "24.05";
 
