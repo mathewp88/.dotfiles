23b08a20ba2c44b0c55f5722cf12a3fe7041ff8d
Mathai Mathew Pulicken <49433106+mathewp88@users.noreply.github.com>
2025-04-25 01:35:56 +0530

    Update

--- nixos/modules/hypr/hyprland.nix
+++ nixos/modules/hypr/hyprland.nix
@@ -1,42 +1,8 @@
 { lib, pkgs, config, ... }:
-# let
-#   sddm-astronaut = pkgs.sddm-astronaut.override {
-#     themeConfig = {
-#       FormPosition = "center";
-#       FullBlur = true;
-#       AccentColor = "#${config.lib.stylix.colors.base05}";
-#       TextColor = "#${config.lib.stylix.colors.base05}";
-#       PlaceholderColor = "#${config.lib.stylix.colors.base05}";
-#       SystemButtonsIconColor = "#${config.lib.stylix.colors.base05}";
-#       HighlightColor = "#${config.lib.stylix.colors.base05}";
-#       Font = "JetBrains Mono Nerd Font Mono";
-#       Background = "${config.stylix.image}";
-#       ForceHideCompletePassword = true;
-#     };
-#   };
-# in
 {
-
-  # services.displayManager.sddm = {
-  #   enable = true;
-  #   package = pkgs.kdePackages.sddm; # qt6 sddm version
-  #   theme = "sddm-astronaut-theme";
-  #   extraPackages = [ sddm-astronaut ];
-  #   wayland.enable = true;
-  # };
-
   services.xserver.displayManager.gdm.enable = true;
 
   services.xserver.desktopManager.gnome.enable = false;
-  environment.gnome.excludePackages = (with pkgs; [
-    gnome-tour
-    gnome-music
-    nautilus
-    gnome-terminal
-    epiphany
-    geary
-    gnome-characters
-  ]);
 
   programs.hyprland = {
     enable = true;
@@ -53,8 +19,8 @@
   security.pam.services.hyprlock.text = lib.readFile "${pkgs.hyprlock}/etc/pam.d/hyprlock";
 
   environment.systemPackages = with pkgs; [
-    # sddm-astronaut
     polkit_gnome # For auth in wm
+    file-roller
   ];
   
   # File Manager Setup
