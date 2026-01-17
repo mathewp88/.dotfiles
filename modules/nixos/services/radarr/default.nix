39e160c62c288b1b6ab0bf5bd5951b53523e677e
Mathai Mathew <49433106+mathewp88@users.noreply.github.com>
2026-01-17 17:50:03 +0530

    Added radarr

---
+++ modules/nixos/services/radarr/default.nix
@@ -0,0 +1,26 @@
+{
+  config,
+  lib,
+  pkgs,
+  namespace,
+  ...
+}:
+with lib;
+with lib.${namespace};
+let
+  cfg = config.${namespace}.services.radarr;
+in
+{
+  options.${namespace}.services.radarr= with types; {
+    enable = mkBoolOpt false "Enable radarr";
+  };
+
+  config = mkIf cfg.enable {
+    services.radarr = {
+      enable = true;
+      openFirewall = true;
+      group = config.services.jellyfin.group;
+      dataDir = "/data/radarr";
+    };
+  };
+}
