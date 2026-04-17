ee58b1a9992bf7fc7c3ae2d7383a3400fd18499e
Mathai Mathew <49433106+mathewp88@users.noreply.github.com>
2026-04-17 17:29:39 +0530

    Remove colemna cachix

--- modules/home/programs/ghostty.nix
+++ modules/home/programs/ghostty.nix
@@ -1,22 +1,21 @@
 {
-  flake.homeModules.ghostty =
-    {
-      home.sessionVariables = {
-        TERMINAL = "ghostty";
-      };
+  flake.homeModules.ghostty = {
+    home.sessionVariables = {
+      TERMINAL = "ghostty";
+    };
 
-      programs.ghostty = {
-        enable = true;
-        enableZshIntegration = true;
-        installBatSyntax = true;
-        installVimSyntax = true;
-        settings = {
-          theme = "Rose Pine";
-          font-size = 14;
-          window-padding-x = 10;
-          window-padding-y = 10;
-          copy-on-select = "clipboard";
-        };
+    programs.ghostty = {
+      enable = true;
+      enableZshIntegration = true;
+      installBatSyntax = true;
+      installVimSyntax = true;
+      settings = {
+        theme = "Rose Pine";
+        font-size = 14;
+        window-padding-x = 10;
+        window-padding-y = 10;
+        copy-on-select = "clipboard";
       };
     };
+  };
 }
